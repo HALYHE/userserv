880604fa4e8dd67f8af10fbff12ace4ec4f2b9f9
HALYHE <mczhenya.bushuev@gmail.com>
2023-12-22 23:13:51 +0300

    del else

--- src/UserService.php
+++ src/UserService.php
@@ -11,8 +11,6 @@ class UserService
         usort($users, function ($a, $b) use ($ascending) {
             if ($ascending) {
                 return strcmp($a->username, $b->username);
-            } else {
-                return strcmp($b->username, $a->username);
             }
         });
 
@@ -24,8 +22,6 @@ class UserService
         usort($users, function ($a, $b) use ($ascending) {
             if ($ascending) {
                 return $a->birthday <=> $b->birthday;
-            } else {
-                return $b->birthday <=> $a->birthday;
             }
         });
 
